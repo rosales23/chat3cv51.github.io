a6ac7f669eeac214a5138eb8fdd36d0d20cacc1e
bewp3cv51 <94018729+bewp3cv51@users.noreply.github.com>
2021-11-11 16:23:51 -0600

    Update init.js

--- js/init.js
+++ js/init.js
@@ -2,11 +2,11 @@
  * configuración en tu servidor de Firebase.  */
 // @ts-ignore
 firebase.initializeApp({
-  apiKey: "AIzaSyDOYMpp68_udyaxhGI6xbtyF0n2HURHnP4",
-  authDomain: "fbauten1001.firebaseapp.com",
-  projectId: "fbauten1001",
-  storageBucket: "fbauten1001.appspot.com",
-  messagingSenderId: "717078882003",
-  appId: "1:717078882003:web:c4c3e1e5a917385cced626",
-  measurementId: "G-3QL4D2NXDN"
+  apiKey: "AIzaSyAknrvw3brgo7qxEyMk3jzsE02CyJKWo5c",
+  authDomain: "fbauten.firebaseapp.com",
+  projectId: "fbauten",
+  storageBucket: "fbauten.appspot.com",
+  messagingSenderId: "902838344264",
+  appId: "1:902838344264:web:221d59cd0ba838db98567d",
+  measurementId: "G-52E0LME3E0"
     });

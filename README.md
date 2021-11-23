2050b01c4c84ef57a3bff8e7ec0e7b511e01994a
bewp3cv51 <bewp3cv51@gmail.com>
2021-11-23 16:45:31 -0600

    Cambio

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# bewp3cv51.github.io
-Ejemplo De Conexión a la B.D con Firebase
+# grupoIc41.github.io
+Ejemplo De Un Poyecto Web con conexión a Firebase con los servicios de:
+- Authentication
+- Cloud Firestore
+- Cloud Storage 
---
+++ js/init.js
@@ -0,0 +1,18 @@
+/*  Inicializa Firebase con la
+ * configuración del proyecto.
+ * Revisa la configuración en tu
+ * servidor de Firebase y cópiala
+ * aquí sustituyendo los
+ * asteriscos. Los campos deben
+ * quedar igual que en tu
+ * servidor. */
+// @ts-ignore
+firebase.initializeApp({
+    apiKey: "AIzaSyCfMMR4bFjJgfECWOsx6iRCHPZTeDQ9GBQ",
+    authDomain: "bdfalumnos41.firebaseapp.com",
+    projectId: "bdfalumnos41",
+    storageBucket: "bdfalumnos41.appspot.com",
+    messagingSenderId: "463512573259",
+    appId: "1:463512573259:web:2531082c01b911490b04ce",
+    measurementId: "G-DXY3ZYMKY4"
+});
